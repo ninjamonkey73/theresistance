8f7cefb48eeb04837ff90757ef40824984f42fda
Amy Brown <ninjamonkey73@gmail.com>
2026-07-18 12:19:16 -0400

    fix: export firebaseConfig for service imports

--- src/app/firebase.config.ts
+++ src/app/firebase.config.ts
@@ -1,12 +1,4 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyCC-PzQckkgv__U5ZzwTiUQOvJ51I3q1gs",
   authDomain: "the-resistance-4e732.firebaseapp.com",
   projectId: "the-resistance-4e732",
@@ -15,7 +7,3 @@ const firebaseConfig = {
   appId: "1:412399247996:web:a3eb2358979dfa48c8dff9",
   measurementId: "G-JTGR02H00Y"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
