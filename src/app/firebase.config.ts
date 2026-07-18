e14cf238a4c3b29947c9af31c377ab270544b904
Amy Brown <ninjamonkey73@gmail.com>
2026-07-18 12:16:44 -0400

    Add Firebase details

--- src/app/firebase.config.ts
+++ src/app/firebase.config.ts
@@ -1,8 +1,21 @@
-export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCC-PzQckkgv__U5ZzwTiUQOvJ51I3q1gs",
+  authDomain: "the-resistance-4e732.firebaseapp.com",
+  projectId: "the-resistance-4e732",
+  storageBucket: "the-resistance-4e732.firebasestorage.app",
+  messagingSenderId: "412399247996",
+  appId: "1:412399247996:web:a3eb2358979dfa48c8dff9",
+  measurementId: "G-JTGR02H00Y"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
